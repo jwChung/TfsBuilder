eb9abb77625081f238af02d3cb6ae17afa53af1f
Jin-Wook Chung <jwChung@hotmail.com>
2013-12-04 17:49:31 +0900

    Introduced the HomeController class.

---
+++ src/WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Jwc.TfsBuilder.WebApplication.Controllers
+{
+    /// <summary>
+    /// Represents the home controller.
+    /// </summary>
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Views the default page.
+        /// </summary>
+        public ActionResult Index()
+        {
+            ////return View();
+            throw new NotImplementedException();
+        }
+	}
+}
---
+++ test/WebApplication.UnitTest/Controllers/HomeControllerTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using Jwc.AutoFixture.Xunit;
+using Xunit;
+
+namespace Jwc.TfsBuilder.WebApplication.Controllers
+{
+    public class HomeControllerTest
+    {
+        [Spec]
+        public void SutIsController(
+            [Build(BuildFlags.NoAutoProperties)] HomeController sut)
+        {
+            Assert.IsAssignableFrom<Controller>(sut);
+        }
+    }
+}
