39df44692f0ef565117ba12b81c128ab36f74228
Jin-Wook Chung <jwChung@hotmail.com>
2013-12-04 21:30:58 +0900

    Configured common build files.

---
+++ build/CommonAssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Jin-Wook Chung")]
+[assembly: AssemblyCopyright("Copyright (c) 2013, Jin-Wook Chung")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.0-pre")]
