d3cfddcb96027618578560c205c13bdb7c6605e0
Jin-Wook Chung <jwChung@hotmail.com>
2013-12-05 01:47:31 +0900

    Introduced the ILogger interface.

---
+++ src/WebApplication/Infrastructure/ILogger.cs
@@ -0,0 +1,15 @@
+﻿namespace Jwc.TfsBuilder.WebApplication.Infrastructure
+{
+    /// <summary>
+    /// Represents a logger.
+    /// </summary>
+    public interface ILogger
+    {
+        /// <summary>
+        /// Logs a message.
+        /// </summary>
+        /// <param name="subject">A subject of the message.</param>
+        /// <param name="body">A body of the message.</param>
+        void Log(string subject, string body);
+    }
+}
