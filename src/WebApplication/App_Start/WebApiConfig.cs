005a5baeb4f7798ce1dc0458ee817316b086bc95
Jin-Wook Chung <jwChung@hotmail.com>
2013-12-04 19:17:02 +0900

    Addressed all the namespaces.

--- src/WebApplication/App_Start/WebApiConfig.cs
+++ src/WebApplication/App_Start/WebApiConfig.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
 
-namespace WebApplication
+namespace Jwc.TfsBuilder.WebApplication
 {
     public static class WebApiConfig
     {
--- src/WebApplication/Global.asax
+++ src/WebApplication/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication.MvcApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Jwc.TfsBuilder.WebApplication.MvcApplication" Language="C#" %>
