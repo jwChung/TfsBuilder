159521000d58f6c2e700746131a5e2f3069b6919
Jin-Wook Chung <jwChung@hotmail.com>
2013-12-05 01:57:09 +0900

    Introduced the new ICommand interface.

---
+++ src/WebApplication/Infrastructure/ICommand.cs
@@ -0,0 +1,15 @@
+﻿namespace Jwc.TfsBuilder.WebApplication.Infrastructure
+{
+    /// <summary>
+    /// Represents a command.
+    /// </summary>
+    /// <typeparam name="T">A type of a comand parameter.</typeparam>
+    public interface ICommand<in T>
+    {
+        /// <summary>
+        /// Executes a command.
+        /// </summary>
+        /// <param name="parameter">A command paramter.</param>
+        void Execute(T parameter);
+    }
+}
