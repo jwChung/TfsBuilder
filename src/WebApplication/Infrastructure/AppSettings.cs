fe6215daada55a07ad3c1d65205a1ebde666bdb3
Jin-Wook Chung <jwChung@hotmail.com>
2013-12-06 00:53:01 +0900

    Implemented the AppSettings class.

---
+++ src/WebApplication/Infrastructure/AppSettings.cs
@@ -0,0 +1,39 @@
+﻿using System.Configuration;
+using System.Dynamic;
+using Microsoft.CSharp.RuntimeBinder;
+
+namespace Jwc.TfsBuilder.WebApplication.Infrastructure
+{
+    /// <summary>
+    /// Represents application settings set from app or web config file.
+    /// </summary>
+    public static class AppSettings
+    {
+        private static readonly DynamicAppSettings _instance = new DynamicAppSettings();
+
+        /// <summary>
+        /// Gets a value indicating a dynamic object to get application settings.
+        /// </summary>
+        public static dynamic Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        private class DynamicAppSettings : DynamicObject
+        {
+            public override bool TryGetMember(GetMemberBinder binder, out object result)
+            {
+                result = ConfigurationManager.AppSettings[binder.Name];
+                return result != null;
+            }
+
+            public override bool TrySetMember(SetMemberBinder binder, object value)
+            {
+                throw new RuntimeBinderException("Supports only read-only properites.");
+            }
+        }
+    }
+}
---
+++ test/WebApplication.UnitTest/Infrastructure/AppSettingsTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Configuration;
+using Jwc.AutoFixture.Xunit;
+using Microsoft.CSharp.RuntimeBinder;
+using Xunit;
+
+namespace Jwc.TfsBuilder.WebApplication.Infrastructure
+{
+    public class AppSettingsTest
+    {
+        [Spec]
+        public void InstanceIsSingleton()
+        {
+            var actual = AppSettings.Instance;
+
+            Assert.NotNull(actual);
+            Assert.Same(AppSettings.Instance, actual);
+        }
+
+        [Spec]
+        public void InstanceHasGmailIdAsDynamic()
+        {
+            var expected = ConfigurationManager.AppSettings["GmailId"];
+
+            var actual = AppSettings.Instance.GmailId;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Spec]
+        public void InstanceHasGoogleAnalyticsTrackingIdAsDynamic()
+        {
+            var expected = ConfigurationManager.AppSettings["GoogleAnalyticsTrackingId"];
+
+            var actual = AppSettings.Instance.GoogleAnalyticsTrackingId;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Spec]
+        public void InstanceThrowsIfDynamicPropertyDoesNotExist()
+        {
+            Assert.Throws<RuntimeBinderException>(() => AppSettings.Instance.InvalidProperty);
+        }
+
+        [Spec]
+        public void InstanceDoesNotSupprtSettingValue(string value)
+        {
+            var e = Assert.Throws<RuntimeBinderException>(
+                () => AppSettings.Instance.GoogleAnalyticsTrackingId = value);
+
+            Assert.Contains("Supports only read-only properites.", e.Message);
+
+        }
+    }
+}
