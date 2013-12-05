d97eacb9db8eb59a97985fa432951de895628a22
Jin-Wook Chung <jwChung@hotmail.com>
2013-12-05 13:11:45 +0900

    Documented the assembly title and description.

--- src/WebApplication/Properties/AssemblyInfo.cs
+++ src/WebApplication/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("WebApplication")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Visual Studio Online Service Hook")]
+[assembly: AssemblyDescription("This project is for a service hook to queue a build process on Visual Studio Online.")]
 [assembly: CLSCompliant(true)]
 [assembly: Guid("4f68884a-67ea-4e0b-a53e-799f5836a299")]
