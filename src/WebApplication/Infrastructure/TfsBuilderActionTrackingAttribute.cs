d519a99571d58a6c981ac8700a27db7e827ea40c
Jin-Wook Chung <jwChung@hotmail.com>
2013-12-05 23:41:38 +0900

    Introduced the TfsBuilderActionTracking attribute.

---
+++ src/WebApplication/Infrastructure/TfsBuilderActionTrackingAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using GoogleAnalyticsTracker.Web.Mvc;
+
+namespace Jwc.TfsBuilder.WebApplication.Infrastructure
+{
+    /// <summary>
+    /// An attribute to provide action tracking of Google analytics.
+    /// </summary>
+    [CLSCompliant(false)]
+    public class TfsBuilderActionTrackingAttribute : ActionTrackingAttribute
+    {
+    }
+}
---
+++ test/WebApplication.UnitTest/Infrastructure/TfsBuilderActionTrackingAttributeTest.cs
@@ -0,0 +1,16 @@
+﻿using GoogleAnalyticsTracker.Web.Mvc;
+using Jwc.AutoFixture.Xunit;
+using Xunit;
+
+namespace Jwc.TfsBuilder.WebApplication.Infrastructure
+{
+    public class TfsBuilderActionTrackingAttributeTest
+    {
+        [Spec]
+        public void SutIsActionTrackingAttribute(
+            [Build(BuildFlags.NoAutoProperties)] TfsBuilderActionTrackingAttribute sut)
+        {
+            Assert.IsAssignableFrom<ActionTrackingAttribute>(sut);
+        }
+    }
+}
