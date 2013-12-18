01263ed60790e39eddd43c65ccd56dbd75040538
Jin-Wook Chung <jwChung@hotmail.com>
2013-12-19 00:14:57 +0900

    Refactored RouteConfigTest.

--- test/WebApplication.UnitTest/RouteConfigTest.cs
+++ test/WebApplication.UnitTest/RouteConfigTest.cs
@@ -3,7 +3,6 @@ using System.Globalization;
 using System.Web;
 using System.Web.Routing;
 using Jwc.AutoFixture.Xunit;
-using Moq;
 using Xunit;
 using Xunit.Extensions;
 
@@ -16,12 +15,10 @@ namespace Jwc.TfsBuilder.WebApplication
             string s1,
             string s2,
             string s3,
-            HttpContextBase httpContext,
             RouteCollection routes)
         {
             var url = string.Format("~/api/{0}/{1}/{2}", s1, s2, s3);
-            Mock.Get(httpContext).Setup(x => x.Request.AppRelativeCurrentExecutionFilePath).Returns(url);
-            Mock.Get(httpContext).Setup(x => x.Request.PathInfo).Returns(string.Empty);
+            var httpContext = new FakeHttpContext(new FakeHttpRequest(url));
 
             RouteConfig.RegisterRoutes(routes);
 
@@ -40,11 +37,9 @@ namespace Jwc.TfsBuilder.WebApplication
         [InlineData("~/home/index")]
         public void RegisterRoutesRegistersHomeIndex(
             string url,
-            HttpContextBase httpContext,
             RouteCollection routes)
         {
-            Mock.Get(httpContext).Setup(x => x.Request.AppRelativeCurrentExecutionFilePath).Returns(url);
-            Mock.Get(httpContext).Setup(x => x.Request.PathInfo).Returns(string.Empty);
+            var httpContext = new FakeHttpContext(new FakeHttpRequest(url));
 
             RouteConfig.RegisterRoutes(routes);
 
@@ -58,17 +53,58 @@ namespace Jwc.TfsBuilder.WebApplication
         public void RegisterRoutesDoesNotRegistersOtherControllerAndAction(
             string controller,
             string action,
-            HttpContextBase httpContext,
             RouteCollection routes)
         {
             var url = string.Format("{0}/{1}", controller, action);
-            Mock.Get(httpContext).Setup(x => x.Request.AppRelativeCurrentExecutionFilePath).Returns(url);
-            Mock.Get(httpContext).Setup(x => x.Request.PathInfo).Returns(string.Empty);
-
+            var httpContext = new FakeHttpContext(new FakeHttpRequest(url));
             RouteConfig.RegisterRoutes(routes);
 
             var routeData = routes.GetRouteData(httpContext);
             Assert.Null(routeData);
+        }
+
+        private class FakeHttpContext : HttpContextBase
+        {
+            private readonly HttpRequestBase _request;
+
+            public FakeHttpContext(HttpRequestBase request)
+            {
+                _request = request;
+            }
+
+            public override HttpRequestBase Request
+            {
+                get
+                {
+                    return _request;
+                }
+            }
+        }
+
+        private class FakeHttpRequest : HttpRequestBase
+        {
+            private readonly string _url;
+
+            public FakeHttpRequest(string url)
+            {
+                _url = url;
+            }
+
+            public override string AppRelativeCurrentExecutionFilePath
+            {
+                get
+                {
+                    return _url;
+                }
+            }
+
+            public override string PathInfo
+            {
+                get
+                {
+                    return string.Empty;
+                }
+            }
         }
     }
 }
