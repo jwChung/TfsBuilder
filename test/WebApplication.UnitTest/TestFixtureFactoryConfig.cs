e9889d5e3e0f7c270cc2f84c9870e37ac47d458c
Jin-Wook Chung <jwChung@hotmail.com>
2014-05-22 15:57:42 +0900

    build: updates Experiment to v0.22.0.

---
+++ test/WebApplication.UnitTest/TestFixtureFactoryConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using Jwc.Experiment.AutoFixture;
+using Jwc.Experiment.Xunit;
+using Jwc.TfsBuilder.WebApplication;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
+
+[assembly: AssemblyFixtureConfig(typeof(TestFixtureFactoryConfig))]
+
+namespace Jwc.TfsBuilder.WebApplication
+{
+    public class TestFixtureFactoryConfig
+    {
+        public TestFixtureFactoryConfig()
+        {
+            DefaultFixtureFactory.SetCurrent(new WebApplicationFixtureFactory());
+        }
+
+        private class WebApplicationFixtureFactory : TestFixtureFactory
+        {
+            protected override IFixture CreateFixture(MethodInfo testMethod)
+            {
+                return base.CreateFixture(testMethod).Customize(new AutoMoqCustomization());
+            }
+        }
+    }
+}
